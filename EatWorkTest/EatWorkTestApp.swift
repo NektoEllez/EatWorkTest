9e4384d3740cfb171b711b75be8c64eeb3207e77
Ivan Potyng <nekto_ellez@gmail.com>
2023-09-13 13:40:16 +0700

    Initial Commit

---
+++ EatWorkTest/EatWorkTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EatWorkTestApp.swift
+//  EatWorkTest
+//
+//  Created by Иван Марин on 13.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct EatWorkTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
